2210f2ea53878f7803ecf7530e4ae9e65433bce3
mariuca <zahariuc_mariuca@yahoo.com>
2024-03-02 20:09:44 +0200

    lesson1

---
+++ lesson1/introduction.js
@@ -0,0 +1,36 @@
+console.log("Hello World!");
+
+
+
+const age = 25; // Number
+const name = "Mariuca"; // String
+const surname = 'Zahariuc';
+const fullName = `${name} ${surname} Salut`;
+const isFemale = true; // Boolean
+
+const mariuca = {
+    name: "Mariuca" ,
+    surname: "Zahariuc"
+}
+
+console.log(mariuca.name);
+
+const ageOfUniversInBilions = 1.8; // not sure about this
+
+let myAge;
+
+myAge = 21;
+
+console.log(myAge);
+
+
+//alert("Are you sure?");
+
+// const areYouSure = confirm("Are you sure?");
+
+// console.log(areYouSure);
+
+// const review = prompt("Please leave a review");
+
+// console.log(review);
+ 
---
+++ lesson1/numbers.js
@@ -0,0 +1,60 @@
+// const a = Number(prompt("Enter fisrt number:"))
+// const b = Number(prompt("Enter second number:"))
+
+let a = 2;
+let b = 4;
+
+const c = a + b;
+console.log("------------------ a=2 b=4 --------------");
+console.log("c = a + b");
+console.log("c =", c);
+console.log("------------------ a=2 b=4 --------------");
+
+
+const sum = a + b;
+
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("------------------ age ------------------ ");
+
+
+let age = 25;
+age = age + 1; 
+/**
+ * another notation for age = age + 1 :
+ * age +=1,
+ * age++,
+ * ++age
+ */
+
+
+console.log("age", age); 
+console.log("------------------ a=2 b=4 --------------");
+
+ 
+console.log("a > b", a > b);
+console.log("a < b", a < b);
+console.log("------------------ a=4 b=4 --------------");
+
+
+a = 4;
+b = 4;
+
+console.log("a >= b", a >= b);
+console.log("a <= b", a <= b);
+
+console.log("a == b", a == b);
+console.log("a === b", a === b);
+
+console.log("------------------ a is a string equal with 2 b=2");
+
+
+a = "2";
+b = 2;
+
+
+console.log("a != b", a != b);
+console.log("a !== b", a !== b);
